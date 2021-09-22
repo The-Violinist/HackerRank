8b124a1fde411ecba4bc2366a9265f1a5041096a
The-Violinist <violinist.david@yahoo.com>
2021-09-22 15:22:39 -0700

    Breaking Records

---
+++ breaking_records.py
@@ -0,0 +1,17 @@
+scores = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]
+
+def breakingRecords(scores):
+    low_score = scores[0]
+    high_score = scores[0]
+    lowest = 0
+    highest = 0
+    for score in scores:
+        if score > high_score:
+            high_score = score
+            highest += 1
+        if score < low_score:
+            low_score = score
+            lowest += 1
+    records = [highest, lowest]
+    return records
+breakingRecords(scores)
