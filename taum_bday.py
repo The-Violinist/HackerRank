11a72eb01179cb4207bdb6e44cc4b45b7c3dc239
The-Violinist <violinist.david@yahoo.com>
2022-03-25 13:31:58 -0700

    Comments for Taum...

--- taum_bday.py
+++ taum_bday.py
@@ -11,16 +11,16 @@ wc = 4                                          # Cost for white gifts
 z = 1                                           # Conversion price
 
 def taumBday(b, w, bc, wc, z):
-    white_total = w*wc
-    black_total = b*bc
-    w_to_b = (b*wc) + (z*b)
-    b_to_w = (w*bc) + (z*w)
-    if w_to_b < black_total:
+    white_total = w*wc                          # Total cost of buying white gifts
+    black_total = b*bc                          # Total cost of buying black gifts
+    w_to_b = (b*wc) + (z*b)                     # Total cost of purchasing white gifts and converting to black
+    b_to_w = (w*bc) + (z*w)                     # Total cost of purchasing clack gifts and converting to white
+    if w_to_b < black_total:                    # Determine if white to black conversion is cheaper
         black_total = w_to_b
-    if b_to_w < white_total:
+    if b_to_w < white_total:                    # Determine if black to white conversion is cheaper
         white_total = b_to_w
     lowest_price = black_total + white_total
-    return lowest_price
+    return lowest_price                         # Return the lowest cost
 
 print(taumBday(b, w, bc, wc, z))
 
